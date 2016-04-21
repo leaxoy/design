73a9e2fc62ebf6615aa0cde9a77b14dba821004e
李晓辉 <lxh@MacBook-Pro.local>
2016-04-21 18:32:07 +0800

    remove auth in home

--- src/main/java/com/example/design/controller/HomeController.java
+++ src/main/java/com/example/design/controller/HomeController.java
@@ -1,16 +1,14 @@
 package com.example.design.controller;
 
-import com.example.design.authorization.annotation.Authorization;
-import com.example.design.constant.UserRole;
 import com.example.design.service.impl.CookingService;
 import com.example.design.service.impl.MenuService;
 import com.example.design.service.impl.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -18,7 +16,7 @@ import javax.servlet.http.HttpServletRequest;
  * 首页的controller
  * Created by lxh on 4/13/16.
  */
-@Controller
+@RestController
 @RequestMapping("")
 public class HomeController {
 
@@ -28,12 +26,9 @@ public class HomeController {
     private MenuService menuService;
     @Autowired
     private CookingService cookingService;
-    @Autowired
-    private RedisTemplate<String, String> redisTemplate;
 
     @RequestMapping(value = "", method = RequestMethod.GET)
-    @Authorization({UserRole.GUEST, UserRole.GUEST})
-    public String home(HttpServletRequest request, Model model) throws Exception {
-        return "index";
+    public ResponseEntity home(HttpServletRequest request, Model model) throws Exception {
+        return ResponseEntity.ok("Hello, world");
     }
 }
