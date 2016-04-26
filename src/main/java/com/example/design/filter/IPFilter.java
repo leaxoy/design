11115c95eb0002e3b35242854c01302ce473bba9
李晓辉 <lixiaohui0812@gmail.com>
2016-04-26 17:27:49 +0800

    add service

--- src/main/java/com/example/design/filter/IPFilter.java
+++ src/main/java/com/example/design/filter/IPFilter.java
@@ -2,7 +2,6 @@ package com.example.design.filter;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.context.annotation.Configuration;
 
 import java.io.IOException;
 
@@ -12,12 +11,13 @@ import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 
 /**
  * IP 过滤器. Created by lxh on 4/13/16.
  */
-@Configuration
+@WebFilter(value = "ipFilter", urlPatterns = "/**")
 public class IPFilter implements Filter {
 
   private static final Logger LOGGER = LoggerFactory.getLogger(IPFilter.class);
