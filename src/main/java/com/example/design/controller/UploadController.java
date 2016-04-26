68e3adb9c9d092b671dded6ee0910f711aaa5bee
李晓辉 <lixiaohui0812@gmail.com>
2016-04-26 20:32:10 +0800

    ^_^, 上次提交搞错了目录...

--- src/main/java/com/example/design/controller/UploadController.java
+++ src/main/java/com/example/design/controller/UploadController.java
@@ -1,8 +1,18 @@
 package com.example.design.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.nio.file.Paths;
 
 /**
  * Created by lxh on 16/4/26.
@@ -11,10 +21,32 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("upload")
 public class UploadController {
 
+  /**
+   * 处理文件上传.
+   *
+   * @param file 文件
+   * @return 返回值
+   */
+  @CrossOrigin
   @RequestMapping(value = "", method = RequestMethod.POST)
-  public String upload() {
+  public ResponseEntity upload(@RequestParam("file") MultipartFile file) {
+    try {
+      // Get the filename and build the local file path (be sure that the
+      // application have write permissions on such directory)
+      String filename = file.getOriginalFilename();
+      String directory = "uploads";
+      String filepath = Paths.get(directory, filename).toString();
 
-    return "";
+      // Save the file locally
+      BufferedOutputStream stream =
+              new BufferedOutputStream(new FileOutputStream(new File(filepath)));
+      stream.write(file.getBytes());
+      stream.close();
+    } catch (Exception exception) {
+      System.out.println(exception.getMessage());
+      return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+    }
+    return new ResponseEntity<>(HttpStatus.OK);
 
   }
 }
