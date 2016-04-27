638a40f404fc9562faeb38380c0eeb572c7fb7b4
ViverZzz <423069614@qq.com>
2016-04-27 16:41:23 +0800

    sk's first commit

--- src/main/java/com/example/design/mapper/MenuLikeMapper.java
+++ src/main/java/com/example/design/mapper/MenuLikeMapper.java
@@ -1,5 +1,9 @@
 package com.example.design.mapper;
 
+import com.example.design.model.MenuLike;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 import org.springframework.stereotype.Repository;
 
 /**
@@ -7,4 +11,43 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public interface MenuLikeMapper {
+    /**
+     * if one user click Like,his(her) behavior will be recorded,and set state as 1
+     *
+     * @param menuLike
+     * @return
+     */
+    @Insert("INSERT INTO menulike(menuId, userId) VALUES (#{menuId}, #{userId})")
+    int addMenuLikeUser(MenuLike menuLike);
+
+    /**
+     * select user's one menu-like record
+     *
+     * @param userId
+     * @param menuId
+     * @return MenuLike
+     */
+    @Select("SELECT * FROM menulike WHERE userId = *{userId} AND menuId = #{menuId}")
+    MenuLike isLike(long userId, long menuId);
+
+    /**
+     * update menuLike's state
+     *
+     * @param like
+     * @param userId
+     * @param menuId
+     * @return
+     */
+    @Update("UPDATE menulike SET state = #{like} WHERE userId = #{userId} AND menuId = #{menuId}")
+    int markMenuLikeState(int like, long userId, long menuId);
+
+    /**
+     * update menu's menuLike
+     *
+     * @param menuId
+     * @param like
+     * @return
+     */
+    @Update("UPDATE menu SET menuLikeNum = menuLikeNum + #{like} WHERE menuId = #{menuId}")
+    int updateLikeOfMenu(long menuId, int like);
 }
--- src/main/java/com/example/design/service/impl/ShowService.java
+++ src/main/java/com/example/design/service/impl/ShowService.java
@@ -1,8 +1,9 @@
 package com.example.design.service.impl;
 
+import com.example.design.mapper.ShowLikeMapper;
 import com.example.design.mapper.ShowMapper;
 import com.example.design.model.Show;
-
+import com.example.design.model.ShowLike;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,35 +15,113 @@ import java.util.List;
 @Service
 public class ShowService {
   @Autowired
-  private ShowMapper showMapper;
+  ShowMapper showMapper;
+  ShowLikeMapper showLikeMapper;
 
+  /**
+   * add one cookingShow to a table
+   *
+   * @param show
+   * @return
+   */
+  public int addShow(Show show) {
+    return showMapper.addShow(show);
+  }
+
+  /**
+   * Update show information
+   *
+   * @param show
+   * @return
+   */
+  public int updateShow(Show show) {
+    return showMapper.updateShow(show);
+  }
+
+  /**
+   * mark show'state as "deleted"
+   *
+   * @param showId
+   * @return
+   */
+  public int markShowDelete(long showId) {
+    return showMapper.markShowDelete(showId);
+  }
+
+  /**
+   * select one show by it's Id
+   *
+   * @param showId
+   * @return Show
+   */
+  public Show findShowById(long showId) {
+    return showMapper.findShowById(showId);
+  }
+
+  /**
+   * select one user's all show
+   *
+   * @param userId
+   * @return List<Show>
+   */
+  public List<Show> findAllShowByUserId(long userId) {
+    return showMapper.findAllShowByUserId(userId);
+  }
+
+  /**
+   * select one cooking's all show
+   *
+   * @param cookingId
+   * @return List<Show>
+   */
+  public List<Show> findAllShowByCookingID(long cookingId) {
+    return showMapper.findAllShowByCookingId(cookingId);
+  }
 
   public List<Show> all() {
-    return showMapper.getAll();
+    return showMapper.all();
   }
 
-
-  public Show id(long id) {
-    return null;
+  /**
+   * *if one user click Like,his(her) behavior will be recorded,and set state as 1
+   *
+   * @param showLike
+   * @return
+   */
+  public int addShowLikeUser(ShowLike showLike) {
+    return showLikeMapper.addShowLikeUser(showLike);
   }
 
-  public List<Show> findByUserId(long userId) {
-    return null;
+  /**
+   * select user's one show-like record
+   * @param userId
+   * @param showId
+   * @return ShowLike
+   */
+  public ShowLike isLike(long userId, long showId) {
+    return showLikeMapper.isLike(userId, showId);
   }
 
-  public List<Show> top10() {
-    return null;
+  /**
+   * update showLike's state
+   *
+   * @param like
+   * @param userId
+   * @param showId
+   * @return
+   */
+  public int markShowLikeState(int like, long userId, long showId) {
+    return showLikeMapper.markShowLikeState(like, userId, showId);
   }
 
-  public int update(Show show) {
-    return 0;
-  }
-
-  public int add(Show show) {
-    return 0;
-  }
-
-  public int delete(Show show) {
-    return 0;
+  /**
+   * update show's showLike
+   *
+   * @param showId
+   * @param like
+   * @return
+   */
+  public int updateLikeOfShow(long showId, int like){
+    return showLikeMapper.updateLikeOfShow(showId, like);
   }
 }
