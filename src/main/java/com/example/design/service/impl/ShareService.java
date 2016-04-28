c7690ec2e2b31295962ccf76dba76e01f0679181
Administrator <592623697@qq.com>
2016-04-28 16:15:23 +0800

    ShareMapper 和 ShareService类的编写

--- src/main/java/com/example/design/service/impl/ShareService.java
+++ src/main/java/com/example/design/service/impl/ShareService.java
@@ -1,10 +1,27 @@
 package com.example.design.service.impl;
 
 import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.example.design.model.Share;
+import com.example.design.mapper.ShareMapper;
+
+import java.util.List;
 
 /**
  * 分享作品的服务接口 Created by lxh on 4/20/16.
  */
 @Service
 public class ShareService {
+    @Autowired
+    private ShareMapper shareMapper;
+
+    public int addShare(Share share) { return shareMapper.addShare(share); }
+
+    public int updateShare(Share share) { return shareMapper.updateShare(share); }
+
+    public int deleteShareByShareId(long shareId) { return shareMapper.deleteByShareId(shareId); }
+
+    public List<Share> selectByUserId(long shareUserId){ return shareMapper.selectByUserId(shareUserId); }
+
 }
