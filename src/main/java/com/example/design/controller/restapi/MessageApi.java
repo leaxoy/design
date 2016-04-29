807af7e87464de10644aff0584f39a00b93e1851
xwpdk2 <2472369610@qq.com>
2016-04-29 09:07:28 +0800

    添加删除查看留言

--- src/main/java/com/example/design/controller/restapi/MessageApi.java
+++ src/main/java/com/example/design/controller/restapi/MessageApi.java
@@ -1,9 +1,10 @@
 package com.example.design.controller.restapi;
 
+import com.example.design.model.Message;
 import com.example.design.service.impl.MessageService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * message rest api.
@@ -23,5 +24,45 @@ public class MessageApi {
      * @param id user id.
      * @return user.
      */
+    /**
+     * 新建留言
+     */
+    @RequestMapping(value = "", method = RequestMethod.POST)
+    public ResponseEntity addMessage(@RequestBody Message message) {
+        /**
+         * 新建留言
+         */
+        int count = messageService.addMessage(message);
+        if (1 == count) {
+            return ResponseEntity.ok(message);
+        }
+        return ResponseEntity.ok("发送失败");
+    }
 
+    @RequestMapping(value = "{messageId}", method = RequestMethod.DELETE)
+    public ResponseEntity deleteMessage(@PathVariable long messageId) {
+        /**
+         * 删除留言
+         */
+        int count = messageService.deleteMessage(messageId);
+        if (1 == count) {
+            return ResponseEntity.ok("删除成功");
+        }
+        return ResponseEntity.ok("修改失败");
+    }
+
+    /**
+     * 获取留言信息
+     *
+     * @param messageId message id
+     * @return message
+     */
+    @RequestMapping(value = "{messageId}", method = RequestMethod.GET)
+    public ResponseEntity changeInfo(@PathVariable long messageId) {
+        Message message = messageService.getByMessageId(messageId);
+        if (message == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(message);
+    }
 }
